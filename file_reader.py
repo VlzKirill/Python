cc509441abd8ebcead5aa6b1b559765d3e2bea62
Kirill Baranov <K.Baranov@aladdin.ru>
2022-05-17 18:19:17 +0300

    initial commit

---
+++ file_reader.py
@@ -0,0 +1,19 @@
+filename = 'pi'
+with open(filename) as file_object:
+#    contents = file_object.read()
+    lines = file_object.readlines()
+#print(contents.rstrip())
+#print(lines)
+pi_string = ''
+for line in lines:
+#    print(line)
+    pi_string += line.strip()
+print(pi_string)
+print(len(pi_string))
+print (f'{pi_string[:50]}')
+
+birthday=input('Enter your birthday in format "ddmmyy":')
+if birthday in pi_string:
+    print ("Your BD is un pi!")
+else:
+    print ("Your BD is not in pi!")
