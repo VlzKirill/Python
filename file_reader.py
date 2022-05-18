94d2efb62312b21fc400795bf7a9cc52f355fc71
Kirill Baranov <K.Baranov@aladdin.ru>
2022-05-18 11:02:47 +0300

    files_ex2

--- file_reader.py
+++ file_reader.py
@@ -4,16 +4,20 @@ with open(filename) as file_object:
     lines = file_object.readlines()
 #print(contents.rstrip())
 #print(lines)
-pi_string = ''
-for line in lines:
+#pi_string = ''
+#for line in lines:
 #    print(line)
-    pi_string += line.strip()
-print(pi_string)
-print(len(pi_string))
-print (f'{pi_string[:50]}')
+#    pi_string += line.strip()
+#print(pi_string)
+#print(len(pi_string))
+#print (f'{pi_string[:50]}')
 
-birthday=input('Enter your birthday in format "ddmmyy":')
-if birthday in pi_string:
-    print ("Your BD is un pi!")
-else:
-    print ("Your BD is not in pi!")
+#birthday=input('Enter your birthday in format "ddmmyy":')
+#if birthday in pi_string:
+#    print ("Your BD is un pi!")
+#else:
+#    print ("Your BD is not in pi!")
+
+with open('py_digits.txt') as testfile:
+    x=testfile.read()
+print (x.replace('3.14','O_O'))
---
+++ write_message.py
@@ -0,0 +1,7 @@
+filename='programming.txt'
+
+with open (filename, 'w') as file_object:
+    file_object.write("I love programming. \n")
+
+with open (filename, 'a') as file_object:
+    file_object.write('Append one more string. \n')
