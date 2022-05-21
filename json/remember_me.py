0de450e6e7595e81483e88ede364f6dbae131cea
Kirill Baranov <K.Baranov@aladdin.ru>
2022-05-21 12:08:13 +0300

    json2

--- json/remember_me.py
+++ json/remember_me.py
@@ -1,13 +1,28 @@
 import json
 
-filename = 'username.json'
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
+def get_stored_username():
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_user():
     username = input("What is ur name? \n")
+    filename = 'username.json'
     with open (filename, 'w') as f:
         json.dump (username, f)
+    return username
+
+def greet_user():
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back? {username}")
+    else:
+        username = get_new_user()
         print(f"We'll remember u when u come back, {username}")
-else:
-    print(f"Welcome back {username}!")
+
+greet_user()
